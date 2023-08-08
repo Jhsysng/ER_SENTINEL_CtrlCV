bea29e1e0ba6accdb1ec3def3efd50d002697bfd
Jeong3733 <wjdwodnr445@naver.com>
2023-08-08 14:30:16 +0900

    Back - SetUp

---
+++ ersentinel_springboot/src/main/java/com/ctrlcv/ersentinel_springboot/ErsentinelSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ctrlcv.ersentinel_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsentinelSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErsentinelSpringbootApplication.class, args);
+    }
+
+}
---
+++ ersentinel_springboot/src/test/java/com/ctrlcv/ersentinel_springboot/ErsentinelSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctrlcv.ersentinel_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErsentinelSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
