8376c97a81497387aa8da964437cc3afe3054d0f
leadawon <dawon337@gmail.com>
2023-08-11 16:59:49 +0900

    Back - ResponseDTO 생성

---
+++ ersentinel_springboot/src/main/java/com/ctrlcv/ersentinel_springboot/data/dto/ResponseDTO.java
@@ -0,0 +1,17 @@
+package com.ctrlcv.ersentinel_springboot.data.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseDTO<T> {
+    private String error;
+    private List<T> data;
+}
